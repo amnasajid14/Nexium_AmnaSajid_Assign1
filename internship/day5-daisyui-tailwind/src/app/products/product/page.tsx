14fe19f945840b2664f1e89c8b06c81f202ca67a
Amna <www.amnasajid144@gmail.com>
2025-07-05 22:52:24 +0500

    day5:Tailwind CSS and DaisyUI components for UI styling

--- internship/day5-daisyui-tailwind/src/app/products/product/page.tsx
+++ internship/day5-daisyui-tailwind/src/app/products/product/page.tsx
@@ -1,7 +1,21 @@
 export default function Home() {
     return (
         <main>
-            My product
+            <div className="hero bg-base-200 min-h-screen">
+  <div className="max-w-5xl hero-content flex-col gap-12 lg:flex-row">
+    <img
+      src="https://northernseeds.ca/cdn/shop/files/shastadaisies.jpg?v=1734314741&width=823"
+      className="max-w-sm rounded-lg shadow-2xl"
+    />
+    <div>
+      <h1 className="text-5xl font-bold">Fresh as a Daisy – Simplicity in Bloom</h1>
+      <p className="py-6">
+        Daisy flowers represent purity, simplicity, and elegance—just like clean UI design. This image brings nature’s minimalism to life, echoing the design principles behind DaisyUI.
+      </p>
+      <button className="btn btn-primary">Explore More</button>
+    </div>
+  </div>
+</div>
         </main>
     )
 }
